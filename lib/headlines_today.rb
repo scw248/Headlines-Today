bcc321e7c815a1fdd207c594d46edbcb4a3085c1
github username <github email address>
2019-02-06 10:12:14 -0600

    scraping dry run and removed duplicitous class

--- lib/headlines_today.rb
+++ lib/headlines_today.rb
@@ -1,10 +1,11 @@
-require_relative "./headlines_today/version"
-require_relative "./headlines_today/cli"
-require_relative "./headlines_today/headline"
-require_relative "./headlines_today/scraper"
-
 require "pry"
 require "nokogiri"
 require "open-uri"
+
+require_relative "./headlines_today/version"
+require_relative "./headlines_today/cli"
+require_relative "./headlines_today/headline"
+
+
 
 
--- lib/headlines_today/headline.rb
+++ lib/headlines_today/headline.rb
@@ -3,19 +3,32 @@ class HeadlinesToday::Headline
     attr_accessor :name, :source_url
 
     def self.today
-
-        @headline = HeadlinesToday::Scraper.scrape_headlines
-
-        # headline_1 = self.new
-        # headline_1.name = "headline name"
-        # headline_1.source_url = "https://www.npr.com"
-
-        # headline_2 = self.new
-        # headline_2.name = "headline name2"
-        # headline_2.source_url = "https://www.npr.com"
-
-        # [headline_1, headline_2]
-
+        self.scrape_headlines
     end
+
+    def self.scrape_headlines
+        headlines = []
+        doc = Nokogiri::HTML(open("https://www.npr.org/?refresh=true"))
+  
+        doc.css(".stories-wrap stories-wrap-featured").each do |headline|
+          headline = self.new
+          headline.name = doc.css(".title").text.strip
+          binding.pry
+          headline.source_url = doc.css(".story-text a").attribute("href").value
+          headlines << headline
+        end
+        headlines
+      end
+
+      def self.scrape_story
+        stories = []
+        self.scrape_headlines.each do |headline|
+          doc = Nokogiri::HTML(open("#{headline.source_url}"))
+  
+          doc.css(".story").each do |story|
+            stories << story.css("p")
+          end
+        end
+      end
 
 end
